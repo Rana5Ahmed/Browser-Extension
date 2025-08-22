44410cbd638de68f4b9ff18842008a2e10b48d3f
Rana5Ahmed <ranaahmedroka@gmail.com>
2025-08-22 18:33:07 +0300

    first commit

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,36 +1,42 @@
-    import logo from '../public/assets/images/logo.svg'
-    import moonIcon from '../public/assets/images/icon-moon.svg';
-    import sunIcon from '../public/assets/images/icon-sun.svg';
-    function NavBar() {
-        let clicked = false;
-        function changeMood() {
-            let iconChange = document.getElementById("icon")
-            clicked = ! clicked
-            if (clicked === true) {
-                iconChange.src = {sunIcon};
-                 document.body.classList.add("dark-mood")
-                 document.body.classList.remove("light-mood")
-                
-            }
-            else{
-                iconChange.src = {moonIcon};
-                document.body.classList.add("light-mood")
-                document.body.classList.remove("dark-mood")
+import React, { useState } from "react";
+import logo from '../public/assets/images/logo.svg';
+import moonIcon from '../public/assets/images/icon-moon.svg';
+import sunIcon from '../public/assets/images/icon-sun.svg';
 
-            }
-            
-           
-            
+function NavBar() {
+    const [clicked, setClicked] = useState(false);
+    const [icon, setIcon] = useState({
+        src: moonIcon,
+        alt: "Moon-Icon"
+    });
+
+    function changeMood() {
+        setClicked(!clicked);
+        if (!clicked) {
+            setIcon({
+                src: sunIcon,
+                alt: "Sun-Icon"
+            });
+            document.body.classList.add("dark-mood");
+            document.body.classList.remove("light-mood");
+        } else {
+            setIcon({
+                src: moonIcon,
+                alt: "Moon-Icon"
+            });
+            document.body.classList.add("light-mood");
+            document.body.classList.remove("dark-mood");
         }
-        return(
-            <>
-            <nav>
-                <img src={logo} alt="Main-Logo" />
-                <button> <img src={moonIcon} alt="Moon-Icon" id="icon" onClick={changeMood}/></button>
-            </nav>
-            
-            </>
-        )
-
     }
-    export default NavBar;
+
+    return (
+        <nav>
+            <img src={logo} alt="Main-Logo" />
+            <button>
+                <img src={icon.src} alt={icon.alt} id="icon" onClick={changeMood} />
+            </button>
+        </nav>
+    );
+}
+
+export default NavBar;
